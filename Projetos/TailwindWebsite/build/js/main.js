6743b363c3f0589a170527e8082d8f3f2f4d80b6
Daniel Moliari <danielmoliaribarbosa@gmail.com>
2023-05-11 16:50:08 -0300

    subindo o site feito com Tailwind

---
+++ Projetos/TailwindWebsite/build/js/main.js
@@ -0,0 +1,15 @@
+const initApp = () => {
+    const hamburguerBtn = document.getElementById('hamburguer-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+        hamburguerBtn.classList.toggle('toggle-btn')
+    }
+
+    hamburguerBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click', toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded', initApp)
